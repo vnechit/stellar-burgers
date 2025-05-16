42db73276449784b580d9b2bd172d57a0d3c8234
vnechit <vnechit310301@gmail.com>
2025-05-16 18:28:36 +0300

    started writing routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,23 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, NotFound404 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  const location = useLocation();
+  console.log(location);
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path='*' element={<NotFound404 />} />
+        <Route path='/' element={<ConstructorPage />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
