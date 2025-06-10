bac8a23dd26786ed92142816d2adbbb807d8ffba
vnechit <vnechit310301@gmail.com>
2025-06-11 01:05:22 +0300

    cypress constructor tests done

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,7 +1,25 @@
+import order from './../fixtures/order.json';
+
 describe('Тестирование конструктора бургеров', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
     cy.visit('http://localhost:4000');
+
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' }).as(
+      'getIngredients'
+    );
+    cy.intercept('GET', 'api/auth/user', { fixture: 'user' }).as('getUser');
+
+    cy.intercept('POST', 'api/orders', { fixture: 'order' }).as('getOrders');
+
+    window.localStorage.setItem('refreshToken', 'refreshToken');
+    cy.setCookie('accessToken', 'accessToken');
+
+    cy.wait('@getIngredients').then((interception) => {
+      expect(interception.response?.statusCode).to.eq(200);
+    });
+    cy.wait('@getUser').then((interception) => {
+      expect(interception.response?.statusCode).to.eq(200);
+    });
   });
 
   it('Доступны ли ингридиенты', () => {
@@ -10,5 +28,60 @@ describe('Тестирование конструктора бургеров', (
       'have.length.at.least',
       1
     );
+  });
+
+  it('проверяет, что localStorage не пуст', () => {
+    cy.getAllLocalStorage().then((localStorage) => {
+      expect(Object.keys(localStorage)).to.have.length.at.least(1);
+    });
+  });
+
+  it('Проверка, что куки не пустые', () => {
+    cy.getCookies().then((cookies) => {
+      expect(cookies).to.not.be.empty;
+    });
+  });
+
+  describe('Работоспособность модальных окон', () => {
+    it('Открытие модального окна и закрытие на крестик', () => {
+      cy.get('[data-ingredient="bun"]:first-of-type').click();
+      cy.wait(1000);
+      cy.get('#modals button:first-of-type').click();
+      cy.get('#modals').children().should('have.length', 0);
+    });
+
+    it('Открытие модального окна и закрытие на оверлей', () => {
+      cy.get('[data-ingredient="bun"]:first-of-type').click();
+      cy.wait(1000);
+      cy.get('#modals>div:nth-of-type(2)').click({ force: true });
+      cy.get('#modals').children().should('have.length', 0);
+    });
+  });
+
+  describe('Работа с ингридиентами и заказом', () => {
+    it('Добавление ингредиентов в бургер', () => {
+      cy.get('[data-ingredient="bun"]:first-of-type button').click();
+      cy.get('[data-ingredient="main"]:first-of-type button').click();
+      cy.get('[data-ingredient="sauce"]:first-of-type button').click();
+    });
+
+    it('Отправка заказа на сервер', () => {
+      cy.get('[data-ingredient="bun"]:first-of-type button').click();
+      cy.get('[data-ingredient="main"]:first-of-type button').click();
+      cy.get('[data-ingredient="sauce"]:first-of-type button').click();
+      cy.get('[data-order-button]').click();
+      cy.get('#modals').children().should('have.length', 2);
+      cy.get('#modals h2:first-of-type').should(
+        'have.text',
+        order.order.number
+      );
+      cy.get('#modals button:first-of-type').click();
+      cy.get('#modals').children().should('have.length', 0);
+    });
+  });
+
+  afterEach(() => {
+    cy.clearAllCookies();
+    cy.clearAllLocalStorage();
   });
 });
--- src/components/ui/burger-constructor/burger-constructor.tsx
+++ src/components/ui/burger-constructor/burger-constructor.tsx
@@ -84,6 +84,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
         size='large'
         children='Оформить заказ'
         onClick={onOrderClick}
+        data-order-button
       />
     </div>
 
